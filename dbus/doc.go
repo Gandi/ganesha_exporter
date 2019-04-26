f498221ac034410c0a2f6424d8050fbe8110c38d
Nicolas Belouin <nicolas.belouin@gandi.net>
2019-04-26 09:52:07 +0200

    Add package documentation for dbus

---
+++ dbus/doc.go
@@ -0,0 +1,8 @@
+/*
+Package dbus act as a simple low-level wrapper for NFS Ganesha dbus calls. It
+follows the documentation of NFS Ganesha available here:
+https://github.com/nfs-ganesha/nfs-ganesha/wiki/Dbusinterface
+
+Please note that for now, some of the defined structures contains elements specific to Gandi version of Ganesha, the fields are marked as such.
+*/
+package dbus
